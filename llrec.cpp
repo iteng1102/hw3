c3e5a2cdb3606c42d2b804871069a5f9501b6d53
Hsiao Jung Teng <iteng@usc.edu>
2025-03-06 23:22:44 +0000

    updated

--- llrec.cpp
+++ llrec.cpp
@@ -4,4 +4,24 @@
 // Provide your implementation of llpivot below
 //*********************************************
 
+void llpivot(Node *&head, Node *&smaller, Node *&larger, int pivot){
+  smaller = nullptr;
+  larger = nullptr; 
+  llpivotHelper(head, smaller, larger, pivot);
+  head = nullptr; 
+}
 
+void llpivotHelper(Node *&head, Node *&smaller, Node *&larger, int pivot){
+  if (head == nullptr){
+    return; //base case return when list is empty 
+  }
+  Node* next = head->next; //set next to the next value 
+  if (head->val > pivot) { 
+    larger = head; //if head value larger than pivot, add it to head 
+    llpivot(next, smaller, larger->next, pivot);
+  } 
+  else if (head->val<= pivot){
+    smaller = head; //if head value larger or equal to pivot, add it to head
+    llpivot(next, smaller->next, larger, pivot);
+  }
+}
